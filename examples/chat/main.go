3f19c50e5f671f019428bebc990f24a6b7fa7d98
Noel Martin <noel@noelmrtn.fr>
2023-07-31 00:01:41 +0200

    basic echo ws

---
+++ examples/chat/main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net/http"
+	"time"
+
+	"nhooyr.io/websocket"
+)
+
+func main() {
+	fn := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		c, err := websocket.Accept(w, r, &websocket.AcceptOptions{OriginPatterns: []string{"*"}})
+		if err != nil {
+			log.Println(err)
+			return
+		}
+		defer c.Close(websocket.StatusInternalError, "the sky is falling")
+
+		ctx, cancel := context.WithTimeout(r.Context(), time.Minute*10)
+		defer cancel()
+
+		recvChan := make(chan []byte)
+		go func() {
+			for {
+				_, msg, err := c.Read(ctx)
+				if err != nil {
+					continue
+				}
+				recvChan <- msg
+			}
+		}()
+
+		chat(c, ctx, recvChan)
+	})
+
+	err := http.ListenAndServe("192.168.1.34:3615", fn)
+	log.Fatal(err)
+}
+
+func chat(c *websocket.Conn, ctx context.Context, recvChan chan []byte) {
+	for {
+		select {
+		case msg := <-recvChan:
+			c.Write(ctx, websocket.MessageBinary, msg)
+		default:
+			continue
+		}
+	}
+}

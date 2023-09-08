5f43657bdb8c2b3a51275d5f8f58eda0bbffc233
NoelM <noel@noelmrtn.fr>
2023-09-08 17:18:46 +0200

    basics of generic pages

--- page.go
+++ page.go
@@ -1,48 +1,163 @@
 package minigo
 
+const NoOp = -100
+const QuitOp = -1
+
+type InitFunc func(mntl *Minitel, inputs InputGroup)
+type KeyboardFunc func(mntl *Minitel, inputs InputGroup, key uint)
+type InChanFunc func(mntl *Minitel, inputs InputGroup, message string)
+type NavigationFunc func(mntl *Minitel, inputs InputGroup) (map[string]string, int)
+
 type Page struct {
-	m *Minitel
+	InChan  chan string
+	OutChan chan string
 
-	initFunc func()
+	mntl   *Minitel
+	name   string
+	inputs InputGroup
+
+	initFunc       InitFunc
+	charFunc       KeyboardFunc
+	inChanFunc     InChanFunc
+	envoiFunc      NavigationFunc
+	sommaireFunc   NavigationFunc
+	annulationFunc NavigationFunc
+	retourFunc     NavigationFunc
+	repetitionFunc NavigationFunc
+	guideFunc      NavigationFunc
+	correctionFunc NavigationFunc
+	suiteFunc      NavigationFunc
 }
 
-func (p *Page) init() {
-	p.initFunc()
+func NewPage(name string, mntl *Minitel) *Page {
+	return &Page{
+		mntl:           mntl,
+		name:           name,
+		initFunc:       func(mntl *Minitel, input InputGroup) { return },
+		charFunc:       func(mntl *Minitel, inputs InputGroup, key uint) { return },
+		inChanFunc:     func(mntl *Minitel, inputs InputGroup, message string) { return },
+		envoiFunc:      defaultNavigationHandlerFunc,
+		sommaireFunc:   defaultNavigationHandlerFunc,
+		annulationFunc: defaultNavigationHandlerFunc,
+		retourFunc:     defaultNavigationHandlerFunc,
+		repetitionFunc: defaultNavigationHandlerFunc,
+		guideFunc:      defaultNavigationHandlerFunc,
+		correctionFunc: defaultNavigationHandlerFunc,
+		suiteFunc:      defaultNavigationHandlerFunc,
+	}
 }
 
-func (p *Page) Run() ([]byte, int) {
+func defaultNavigationHandlerFunc(mntl *Minitel, input InputGroup) (map[string]string, int) {
+	return nil, NoOp
+}
 
-	p.init()
+func (p *Page) SetInitFunc(f InitFunc) {
+	p.initFunc = f
+}
+
+func (p *Page) SetCharFunc(f KeyboardFunc) {
+	p.charFunc = f
+}
+
+func (p *Page) SetEnvoiFunc(f NavigationFunc) {
+	p.envoiFunc = f
+}
+
+func (p *Page) SetSommaireFunc(f NavigationFunc) {
+	p.sommaireFunc = f
+}
+
+func (p *Page) SetAnnulationFunc(f NavigationFunc) {
+	p.annulationFunc = f
+}
+
+func (p *Page) SetRetourFunc(f NavigationFunc) {
+	p.retourFunc = f
+}
+
+func (p *Page) SetRepetitionFunc(f NavigationFunc) {
+	p.repetitionFunc = f
+}
+
+func (p *Page) SetGuideFunc(f NavigationFunc) {
+	p.guideFunc = f
+}
+
+func (p *Page) SetCorrectionFunc(f NavigationFunc) {
+	p.correctionFunc = f
+}
+
+func (p *Page) SetSuiteFunc(f NavigationFunc) {
+	p.suiteFunc = f
+}
+
+func (p *Page) SetInChanFunc(f InChanFunc) {
+	p.inChanFunc = f
+}
+
+func (p *Page) Run() (map[string]string, int) {
+
+	p.initFunc(p.mntl, p.inputs)
 
 	for {
 		select {
-		case key := <-m.RecvKey:
-			if key == minigo.Envoi {
-				if len(nickInput.Value) == 0 {
-					warnLog.Println("Empty nick input")
-					continue
+		case msg := <-p.InChan:
+			p.inChanFunc(p.mntl, p.inputs, msg)
+
+		case key := <-p.mntl.RecvKey:
+			switch key {
+			case Envoi:
+				if out, op := p.envoiFunc(p.mntl, p.inputs); op != NoOp {
+					return out, op
 				}
-				m.Reset()
 
-				infoLog.Printf("Logged as: %s\n", nickInput.Value)
-				return nickInput.Value, noopId
+			case Sommaire:
+				if out, op := p.sommaireFunc(p.mntl, p.inputs); op != NoOp {
+					return out, op
+				}
 
-			} else if key == minigo.Correction {
-				nickInput.Correction()
+			case Annulation:
+				if out, op := p.annulationFunc(p.mntl, p.inputs); op != NoOp {
+					return out, op
+				}
 
-			} else if key == minigo.Sommaire {
-				return nil, sommaireId
+			case Retour:
+				if out, op := p.retourFunc(p.mntl, p.inputs); op != NoOp {
+					return out, op
+				}
 
-			} else if minigo.IsUintAValidChar(key) {
-				nickInput.AppendKey(byte(key))
+			case Repetition:
+				if out, op := p.repetitionFunc(p.mntl, p.inputs); op != NoOp {
+					return out, op
+				}
 
-			} else {
-				errorLog.Printf("Not supported key: %d\n", key)
+			case Guide:
+				if out, op := p.guideFunc(p.mntl, p.inputs); op != NoOp {
+					return out, op
+				}
+
+			case Correction:
+				if out, op := p.correctionFunc(p.mntl, p.inputs); op != NoOp {
+					return out, op
+				}
+
+			case Suite:
+				if out, op := p.suiteFunc(p.mntl, p.inputs); op != NoOp {
+					return out, op
+				}
+
+			default:
+				if IsUintAValidChar(key) {
+					p.charFunc(p.mntl, p.inputs, key)
+
+				} else {
+					errorLog.Printf("not supported key: %d\n", key)
+				}
+
 			}
-
-		case <-m.Quit:
-			warnLog.Println("Quitting log page")
-			return nil, quitId
+		case <-p.mntl.Quit:
+			warnLog.Printf("quitting %s page\n", p.name)
+			return nil, QuitOp
 
 		default:
 			continue

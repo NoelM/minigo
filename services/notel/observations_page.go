9af840d699bc41493d141441097db4783cd889f6
Noel Martin <noel@noelmrtn.fr>
2023-09-10 16:46:09 +0200

    lisibilite

--- services/notel/observations_page.go
+++ services/notel/observations_page.go
@@ -83,21 +83,21 @@ func printWeatherReport(mntl *minigo.Minitel, rep WeatherReport) {
 
 func windDirToString(deg float64) string {
 	if deg > 337.5 || deg <= 22.5 {
-		return "N"
+		return "NORD"
 	} else if deg > 22.5 && deg <= 67.5 {
-		return "NE"
+		return "NORD-EST"
 	} else if deg > 67.5 && deg <= 112.5 {
-		return "E"
+		return "EST"
 	} else if deg > 112.5 && deg <= 157.5 {
-		return "SE"
+		return "SUD-EST"
 	} else if deg > 157.5 && deg <= 202.5 {
-		return "S"
+		return "SUD"
 	} else if deg > 202.5 && deg <= 247.5 {
-		return "SO"
+		return "SUD-OUEST"
 	} else if deg > 247.5 && deg <= 292.5 {
-		return "O"
+		return "OUEST"
 	} else if deg > 292.5 && deg <= 337.5 {
-		return "NO"
+		return "NORD-OUEST"
 	} else {
 		return ""
 	}
--- services/notel/previ_page.go
+++ services/notel/previ_page.go
@@ -109,9 +109,10 @@ func printForecast(mntl *minigo.Minitel, f Forecast, date string, c *Commune) {
 
 	mntl.WriteStringXY(1, 6, nebulositeToString(f.Nebulosite.Totale))
 
-	mntl.WriteStringXY(1, 8, fmt.Sprintf("TEMP:  %.0f C", f.Temperature.TwoM-275.))
-	mntl.WriteStringXY(1, 9, fmt.Sprintf("VENT:  %.0f km/h - DIRECTION: %s", f.VentMoyen.One0M*3.6, windDirToString(f.VentDirection.One0M)))
-	mntl.WriteStringXY(1, 10, fmt.Sprintf("PLUIE: %f mm", f.Pluie))
+	mntl.WriteStringXY(1, 8, fmt.Sprintf("TEMP: %.0f C", f.Temperature.TwoM-275.))
+	mntl.WriteStringXY(1, 10, fmt.Sprintf("VENT: %.0f km/h - RAFALES: %.0f", f.VentMoyen.One0M*3.6, f.VentRafales.One0M*3.6))
+	mntl.WriteStringXY(1, 11, fmt.Sprintf("DIR:  %s", windDirToString(f.VentDirection.One0M)))
+	mntl.WriteStringXY(1, 13, fmt.Sprintf("PLUIE: %.0f mm", f.Pluie))
 
 	mntl.WriteStringXY(1, 24, "DONNEES: INFO-CLIMAT")
 }

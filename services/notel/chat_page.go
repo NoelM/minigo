e4043615e93ec135f076d3c8f83f7cd2119c259d
Noel Martin <noel@noelmrtn.fr>
2023-09-13 22:03:27 +0200

    fixing rouleau

--- services/notel/chat_page.go
+++ services/notel/chat_page.go
@@ -17,11 +17,11 @@ func NewChatPage(m *minigo.Minitel, ircDrv *IrcDriver) *minigo.Page {
 		subscriberId = MessageDb.Subscribe()
 		inputs.AppendInput("messages", minigo.NewInput(m, 1, InputLine, 40, 5, ">", true))
 
+		m.RouleauOn()
 		updateScreen(m, subscriberId)
 		helpers(m)
 
 		inputs.RepetitionActive()
-		m.RouleauOn()
 
 		return minigo.NoOp
 	})

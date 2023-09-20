a92a31eab6466f32fe9ebd4912701c9c41171f2c
Noel Martin <noel@noelmrtn.fr>
2023-09-20 13:59:11 +0200

    adding dates

--- services/notel/previ_page.go
+++ services/notel/previ_page.go
@@ -186,3 +186,33 @@ func weekDayIdToString(i time.Weekday) string {
 	}
 	return ""
 }
+
+func monthIdToString(i time.Month) string {
+	switch i {
+	case time.January:
+		return "Janvier"
+	case time.February:
+		return "Février"
+	case time.March:
+		return "Mars"
+	case time.April:
+		return "Avril"
+	case time.May:
+		return "Mai"
+	case time.June:
+		return "Juin"
+	case time.July:
+		return "Juillet"
+	case time.August:
+		return "Août"
+	case time.September:
+		return "Septembre"
+	case time.October:
+		return "Octobre"
+	case time.November:
+		return "Novembre"
+	case time.December:
+		return "Décembre"
+	}
+	return ""
+}

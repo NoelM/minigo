7d5366af050735e0caec7f02e2579b661c677808
NoelM <noel@noelmrtn.fr>
2023-11-26 19:58:26 +0100

    comments and logs

--- network.go
+++ network.go
@@ -5,6 +5,10 @@ import (
 	"time"
 )
 
+// 1 byte, is 8 bits and 2 bits (start and stop)
+// 1 byte = 10 symbols
+const ByteDurAt1200Bd = 8333 * time.Microsecond
+
 type Network struct {
 	conn   Connector
 	parity bool
@@ -74,6 +78,7 @@ func (n *Network) listenLoop() {
 			if b == Sub {
 				// SUB desginates parity error, we increment counter wetherer
 				// the PCE starts or not
+				warnLog.Printf("[%s] listen: minitel bad parity with SUB\n", n.source)
 				n.incSub()
 
 				// The byte is not sent to the minitel
@@ -83,6 +88,14 @@ func (n *Network) listenLoop() {
 			if b == Nack {
 				// The minitel requested a repetition, noted:
 				// NACK, X, with X the block to be repeated
+				if !n.pce {
+					errorLog.Printf("[%s] listen: minitel request sync (NACK) but PCE is OFF\n", n.source)
+
+					// The byte is not sent to the minitel loop
+					continue
+				}
+
+				infoLog.Printf("[%s] listen: minitel request synchronization NACK frame\n", n.source)
 				n.nackTime = time.Now()
 				n.nackBlock = true
 
@@ -96,6 +109,10 @@ func (n *Network) listenLoop() {
 					// Irrelevant block ID, bye, bye
 					n.nackBlockId = b - byte(0x40)
 					n.nackSynSend = true
+
+					infoLog.Printf("[%s] listen: receieved NACK block=%x\n", n.source, b)
+				} else {
+					errorLog.Printf("[%s] listen: receieved invalid NACK block=%x\n", n.source, b)
 				}
 				n.nackBlock = false
 
@@ -113,6 +130,8 @@ func (n *Network) listenLoop() {
 				// * next if it needs another byte, set to false if the
 				//   suite of bytes are irrelevant
 				if ack, next := AckPCE(pceAck); ack {
+					infoLog.Printf("[%s] listen: minitel acknowledged the PCE\n", n.source)
+
 					n.pce = true
 					n.pcePending = false
 					pceAck = []byte{}
@@ -120,6 +139,8 @@ func (n *Network) listenLoop() {
 				} else if !next {
 					// If next is at false, without an ack, this means that the suite
 					// of bytes does not correspond to an ack
+					warnLog.Printf("[%s] listen: unable to acknowledge PCE, invalid message\n", n.source)
+
 					n.pcePending = false
 					pceAck = []byte{}
 				}
@@ -137,10 +158,13 @@ func (n *Network) incSub() {
 	if time.Since(n.subTime) < time.Minute {
 		// If the counter has been reset less than a miniute ago, we increment
 		n.subCnt += 1
+		infoLog.Printf("[%s] inc-sub: increment SUB counter=%d since=%.0f sec\n", n.source, n.subCnt, time.Since(n.subTime).Seconds())
 
 		if n.subCnt > MaxSubPerMinute && !n.pce && !n.pcePending {
 			// If the limit is reached, and the PCE not active nor Pending (waiting for the ack)
 			// the module asks for PCE ON
+			warnLog.Printf("[%s] inc-sub: the activation of PCE has been transmitted\n", n.source)
+
 			n.send(GetRequestPCE())
 			n.pcePending = true
 		}
@@ -148,13 +172,15 @@ func (n *Network) incSub() {
 		// The last reset was more than a minute ago! So we reset the counters
 		n.subTime = time.Now()
 		n.subCnt = 1
+
+		infoLog.Printf("[%s] inc-sub: reset SUB counter %d\n", n.source, n.subCnt)
 	}
 }
 
 func (n *Network) sendLoop() {
 	for n.conn.Connected() {
 		if n.nackBlock || n.pcePending {
-			// Some commands block all the Send commands:
+			// Some commands block all the Send data:
 			// * NACK, waits for a blockId
 			// * PCE ON, waits for the ack
 			time.Sleep(100 * time.Millisecond)
@@ -166,9 +192,13 @@ func (n *Network) sendLoop() {
 			// If the block id has been received, we try to repeat the last block
 
 			if n.pceCache.Has(n.nackBlockId) {
-				// If the block is in cache, well done, we send it
+				// The block is in cache, well done, we send it
 				n.send(GetSynFrame(n.nackBlockId, n.pceCache.Get(n.nackBlockId)))
+				infoLog.Printf("[%s] send: the SYN frame has been sent for block=%x\n", n.source, n.nackBlockId+0x40)
 
+				// When NACK is sent, the minitel starts a counter of 1140ms (NackTimer),
+				// we release the loop when the time is elapsed, in order to avoid
+				// other messages sent while the minitel waits for the SYN frame
 				time.Sleep(time.Since(n.nackTime) - NackTimer)
 				n.nackSynSend = false
 
@@ -179,9 +209,20 @@ func (n *Network) sendLoop() {
 
 			select {
 			case blk := <-n.pceStack:
+				// The block is not in cache, we get the fist block to send
+				// It should only be the block 0x40
 				n.pceCache.Add(blk)
 				n.send(GetSynFrame(n.nackBlockId, blk))
 
+				if n.nackBlockId == 0 {
+					infoLog.Printf("[%s] send: the SYN frame has been sent with the first PCE\n", n.source)
+				} else {
+					warnLog.Printf("[%s] send: the SYN frame has been sent with the first PCE (block=%x)\n", n.source, n.nackBlockId+0x40)
+				}
+
+				// When NACK is sent, the minitel starts a counter of 1140ms (NackTimer),
+				// we release the loop when the time is elapsed, in order to avoid
+				// other messages sent while the minitel waits for the SYN frame
 				time.Sleep(time.Since(n.nackTime) - NackTimer)
 				n.nackSynSend = false
 
@@ -198,6 +239,8 @@ func (n *Network) sendLoop() {
 		case msg := <-n.Out:
 			if n.pce {
 				// when the PCE is active, we compute blocks
+				// but we always send them later, to prevent any
+				// NACK requested after a block
 				for _, blk := range ApplyPCE(msg) {
 					n.pceStack <- blk
 				}
@@ -214,6 +257,7 @@ func (n *Network) sendLoop() {
 
 		// PCE send blocks from pceStack
 		if n.pce && !n.nackSynSend {
+			// The PCE is ON and no NACK/SYN from has been requested
 			// We pop the first-inserted block, and send it if exists
 			select {
 			case blk := <-n.pceStack:
@@ -228,6 +272,7 @@ func (n *Network) sendLoop() {
 
 func (n *Network) send(data []byte) {
 	n.conn.Write(data)
+	time.Sleep(time.Duration(len(data)) * ByteDurAt1200Bd)
 }
 
 func (n *Network) Connected() bool {

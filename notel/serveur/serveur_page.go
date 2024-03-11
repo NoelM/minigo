098c7128f2bab0b6cef94c75bd54236b11c29343
noel <noel@noelmrtn.fr>
2024-03-11 23:39:42 +0100

    nitpicks on SRV

--- notel/serveur/serveur_page.go
+++ notel/serveur/serveur_page.go
@@ -14,68 +14,78 @@ func NewServeurPage(mntl *minigo.Minitel) *minigo.Page {
 		mntl.CleanScreen()
 		mntl.CursorOff()
 
-		mntl.MoveAt(2, 1)
-		mntl.Attributes(minigo.DoubleHauteur)
-		mntl.Print("Serveur")
-		mntl.Attributes(minigo.DoubleGrandeur, minigo.InversionFond)
-
-		mntl.Right(1)
-		mntl.Print("NOTEL")
-		mntl.Attributes(minigo.GrandeurNormale, minigo.FondNormal)
+		mntl.MoveAt(2, 0)
+		mntl.Attributes(minigo.DoubleHauteur, minigo.CaractereJaune)
+		mntl.PrintCenter("Serveur NOTEL")
+		mntl.Attributes(minigo.GrandeurNormale)
 
 		mntl.Return(1)
 		mntl.HLine(40, minigo.HCenter)
 
-		mntl.Return(2)
-		mntl.PrintAttributes("→ Cette semaine", minigo.DoubleHauteur)
-		mntl.Return(2)
+		mntl.RectAt(4, 1, 38, 8)
+
+		mntl.MoveAt(5, 0)
+		mntl.Attributes(minigo.CaractereVert)
+		mntl.PrintCenter("Stats de la semaine")
+
+		mntl.ReturnCol(1, 2)
+		mntl.HLine(36, minigo.Top)
+		mntl.Attributes(minigo.CaractereBlanc)
 
 		if r, err := Request(ConnectWeekly); err == nil {
-			mntl.PrintAttributes(" Connexions ", minigo.InversionFond)
+			mntl.ReturnCol(1, 2)
+			mntl.Print("Connexions")
+			mntl.Right(2)
 
 			f, _ := strconv.ParseFloat(r.Data.Result[0].Value[1].(string), 64)
-			mntl.PrintAttributes(fmt.Sprintf(" %.0f", f), minigo.CaractereVert)
-
-			mntl.Return(1)
-			mntl.PrintAttributes("(connexions de tests comprises)", minigo.CaractereRouge)
+			mntl.PrintAttributes(fmt.Sprintf("%.0f", f), minigo.CaractereVert)
 		}
 
 		if r, err := Request(DurationWeekly); err == nil {
-			mntl.Return(2)
-			mntl.PrintAttributes(" Durées ", minigo.InversionFond)
+			mntl.ReturnCol(1, 2)
+			mntl.Print("Durée")
+			mntl.Right(7)
 
 			f, _ := strconv.ParseFloat(r.Data.Result[0].Value[1].(string), 64)
-			mntl.PrintAttributes(fmt.Sprintf(" %.1f heures", f/3600.), minigo.CaractereVert)
+			mntl.PrintAttributes(fmt.Sprintf("%.0f H", f/3600.), minigo.CaractereVert)
 		}
 
 		if r, err := Request(MessagesWeekly); err == nil {
-			mntl.Return(2)
-			mntl.PrintAttributes(" Messages ", minigo.InversionFond)
+			mntl.ReturnCol(1, 2)
+			mntl.Print("Messages")
+			mntl.Right(4)
 
 			f, _ := strconv.ParseFloat(r.Data.Result[0].Value[1].(string), 64)
-			mntl.PrintAttributes(fmt.Sprintf(" %.0f", f), minigo.CaractereVert)
+			mntl.PrintAttributes(fmt.Sprintf("%.0f", f), minigo.CaractereVert)
 		}
 
-		mntl.Return(2)
-		mntl.HLine(40, minigo.HCenter)
+		mntl.RectAt(13, 1, 38, 7)
 
-		mntl.Return(2)
-		mntl.PrintAttributes("→ En ce moment", minigo.DoubleHauteur)
+		mntl.MoveAt(14, 0)
+		mntl.Attributes(minigo.CaractereMagenta)
+		mntl.PrintCenter("Direct Live")
+
+		mntl.ReturnCol(1, 2)
+		mntl.HLine(36, minigo.Top)
+
+		mntl.Attributes(minigo.CaractereBlanc)
 
 		if r, err := Request(CPULoad); err == nil {
-			mntl.Return(2)
-			mntl.PrintAttributes(" Processeur ", minigo.InversionFond)
+			mntl.ReturnCol(1, 2)
+			mntl.Print("Processeur")
+			mntl.Right(2)
 
 			f, _ := strconv.ParseFloat(r.Data.Result[0].Value[1].(string), 64)
-			mntl.PrintAttributes(fmt.Sprintf(" %.1f %%", f), minigo.CaractereVert)
+			mntl.PrintAttributes(fmt.Sprintf("%.1f %%", f), minigo.CaractereMagenta)
 		}
 
 		if r, err := Request(CPUTemp); err == nil {
-			mntl.Return(2)
-			mntl.PrintAttributes(" Température ", minigo.InversionFond)
+			mntl.ReturnCol(1, 2)
+			mntl.Print("Température")
+			mntl.Right(1)
 
 			f, _ := strconv.ParseFloat(r.Data.Result[0].Value[1].(string), 64)
-			mntl.PrintAttributes(fmt.Sprintf(" %.1f °C", f), minigo.CaractereVert)
+			mntl.PrintAttributes(fmt.Sprintf("%.1f °C", f), minigo.CaractereMagenta)
 		}
 
 		mntl.HelperLeftAt(24, "Menu NOTEL", "SOMMAIRE")

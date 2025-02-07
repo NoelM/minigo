31950f349b2ec6f3573b3c9d97ba464900351606
Noel Martin <noel@noelmrtn.fr>
2025-02-07 23:17:26 +0100

    wait for connect validation

--- notel/tcp.go
+++ notel/tcp.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net"
+	"regexp"
 	"sync"
+	"time"
 
 	"github.com/NoelM/minigo"
 	"github.com/NoelM/minigo/notel/confs"
@@ -17,21 +20,30 @@ func serveTCP(wg *sync.WaitGroup, connConf confs.ConnectorConf, metrics *Metrics
 		tcp, _ := minigo.NewTCP(conn)
 		_ = tcp.Init()
 
+		number, err := waitForConnect(tcp)
+		if err != nil {
+			logs.ErrorLog("[%s] serve-TCP: unable to connect %s\n", err)
+			return
+		}
+		logs.InfoLog("[%s] serve-TCP: connect from number %s\n", connConf.Tag, number)
+
+		fullTag := fmt.Sprintf("%s:%s", connConf.Tag, number)
+
 		var innerWg sync.WaitGroup
 		innerWg.Add(2)
 
 		network := minigo.NewNetwork(tcp, false, &innerWg, "TCP")
-		m := minigo.NewMinitel(network, false, &innerWg, connConf.Tag, metrics.ConnLostCount)
+		m := minigo.NewMinitel(network, false, &innerWg, fullTag, metrics.ConnLostCount)
 		m.NoCSI()
 		go m.Serve()
 
 		NotelApplication(m, &innerWg, &connConf, metrics)
 		innerWg.Wait()
 
-		logs.InfoLog("[%s] serve-TCP: disconnect\n", connConf.Tag)
+		logs.InfoLog("[%s] serve-TCP: disconnect\n", fullTag)
 		tcp.Disconnect()
 
-		logs.InfoLog("[%s] serve-TCP: session closed\n", connConf.Tag)
+		logs.InfoLog("[%s] serve-TCP: session closed\n", fullTag)
 	}
 
 	err := listenAndServeTCP(connConf.Path, handler)
@@ -54,4 +66,31 @@ func listenAndServeTCP(path string, handler func(net.Conn)) error {
 
 		go handler(conn)
 	}
+}
+
+func waitForConnect(tcp *minigo.TCP) (string, error) {
+	/*
+		CALLFROM 0173742367
+		STARTURL
+		PCE 1
+	*/
+
+	msg := make([]byte, 0)
+	re := regexp.MustCompile(`CALLFROM\s(\d+)\nSTARTURL\nPCE\s\d`)
+
+	start := time.Now()
+	for time.Since(start) < time.Minute {
+		part, err := tcp.Read()
+		if err != nil {
+			return "", err
+		}
+
+		msg = append(msg, part...)
+
+		if re.Match(msg) {
+			return re.FindStringSubmatch(msg)[0], nil
+		}
+	}
+
+	return "", fmt.Errorf("unable to connect V.23")
 }

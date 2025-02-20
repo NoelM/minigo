beee980959a98ff15f8d49591bd5ba673a77a60e
NoelM <mail@noelmrtn.fr>
2025-02-20 09:42:58 +0100

    free with phone number

--- notel/annuaire/annuaire_page.go
+++ notel/annuaire/annuaire_page.go
@@ -56,7 +56,7 @@ func NewPageAnnuaire(mntl *minigo.Minitel, annuaireDbPath string) *minigo.Page {
 			// 2 |          |15
 			//   len(name)
 
-			mntl.Repeat(' ', 21-len(item.Name))
+			mntl.Repeat(' ', 26-len(item.Name)-len(item.Phone))
 			mntl.Printf("%s ", item.Phone)
 		}
 

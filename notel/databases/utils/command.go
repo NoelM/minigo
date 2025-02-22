b64b066a60b58b005ec9f47e59f9cdf6b7ef01cc
Noel Martin <noel@noelmrtn.fr>
2025-02-22 15:50:36 +0100

    added a user list feature

--- notel/databases/utils/command.go
+++ notel/databases/utils/command.go
@@ -19,9 +19,62 @@ func main() {
 		return
 	}
 
-	dbPath := os.Args[1]
-	nick := os.Args[2]
-	pwd := os.Args[3]
+	command := os.Args[1]
+
+	switch command {
+	case "pass":
+		ChangePassword(os.Args[1:])
+
+	case "users":
+		ListUsers(os.Args[1:])
+
+	case "help":
+		fmt.Println("help")
+		fmt.Println("./utils pass [user-db-path] [nickname] [password]")
+		fmt.Println("./utils users [user-db-path]")
+
+	default:
+		fmt.Println("no match")
+	}
+
+}
+
+func ListUsers(args []string) {
+	dbPath := args[1]
+
+	usersDb := databases.NewUsersDatabase()
+	usersDb.LoadDatabase(dbPath)
+
+	users, err := usersDb.LoadAllUsers()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, u := range users {
+		fmt.Printf("> %s\n", u.Nick)
+		fmt.Printf("  %s\n", u.LastConnect.Format("02/01/2006 15:04:05"))
+		fmt.Printf("  %s\n", u.Bio)
+		fmt.Printf("  %s\n", u.PwdHash)
+		fmt.Printf("  %s\n", u.Location)
+		fmt.Printf("  %s\n", u.Tel)
+		fmt.Printf("  Rep: %t\n", u.AppAnnuaire)
+	}
+
+}
+
+func ChangePassword(args []string) {
+	// os.Args is a slice of strings that contains all the command-line arguments.
+	// The first element, os.Args[0], is the path to the executable.
+	// The subsequent elements are the arguments passed to the program.
+
+	if len(os.Args) < 2 {
+		fmt.Println("Please provide some command-line arguments.")
+		return
+	}
+
+	dbPath := args[1]
+	nick := args[2]
+	pwd := args[3]
 
 	usersDb := databases.NewUsersDatabase()
 	usersDb.LoadDatabase(dbPath)
